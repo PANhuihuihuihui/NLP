a31ea47bd3a1fd5d44554c03a3c3d879c67d960c
PANhuihuihuihui <phjhk@connect.hku.hk>
2021-03-06 20:27:13 +0800

    A

--- merge/data_prepare.py
+++ merge/data_prepare.py
@@ -49,4 +49,5 @@ train:  8000 val:  1000 test:  1656
 
 # check the oov and eos index
 for i in range(50):
-    print(gVocab.tgt_vocab.id2word[i+15000] == gVocab.src_vocab.id2word[i])
+    print(gVocab.tgt_vocab.id2word.keys())
+    #gVocab.src_vocab.id2word[i]
