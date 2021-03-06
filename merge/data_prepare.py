adc2afb1d8b1436f9ef2ec1eca5d032671e1c682
PANhuihuihuihui <phjhk@connect.hku.hk>
2021-03-06 20:38:05 +0800

    A

--- merge/data_prepare.py
+++ merge/data_prepare.py
@@ -54,5 +54,4 @@ key1 = list(gVocab.src_vocab.id2word.keys())
 print(key1[0:100])
 
 for i in range(50):
-    print(gVocab.src_vocab.id2word[str(i)])
-    print(gVocab.tgt_vocab.id2word[str(i)])
+    print(gVocab.src_vocab.id2word[str(i)] == gVocab.tgt_vocab.id2word[str(i)])
