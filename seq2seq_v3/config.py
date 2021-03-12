a1f216221a58f7c811c785ba33f97efba8e26a47
PANhuihuihuihui <phjhk@connect.hku.hk>
2021-03-12 23:47:20 +0800

    A

--- seq2seq_v3/config.py
+++ seq2seq_v3/config.py
@@ -5,18 +5,19 @@ from typing import Optional
 import torch
 
 # General
-hidden_size: int = 400
-dec_hidden_size: Optional[int] = 400
+hidden_size: int = 300
+dec_hidden_size: Optional[int] = 300
 embed_size: int = 100
 pointer = True
 
 #
+
 model_name ="seq2seq"
-encoder_save_name = "encoder_bi_400"
-decoder_save_name ="decoder_ni_400"
-attention_save_name = "attention_400"
-reduce_state_save_name = "reduce_400"
-vocab_save_name = "vocab_400"
+encoder_save_name = "encoder_bi_{}".format(hidden_size)
+decoder_save_name ="decoder_ni_{}".format(hidden_size)
+attention_save_name = "attention_{}".format(hidden_size)
+reduce_state_save_name = "reduce_{}".format(hidden_size)
+vocab_save_name = "vocab_{}".format(hidden_size)
 
 # Data
 max_vocab_size = 30000
@@ -25,14 +26,14 @@ source = 'train'    # use value: train or  big_samples
 data_path: str = 'files/{}.txt'.format(source)
 val_data_path: Optional[str] = 'files/val.txt'
 test_data_path: Optional[str] = 'files/test.txt'
-max_skill_len: int = 50  # exclusive of special tokens such as EOS
+max_skill_len: int = 2  # exclusive of special tokens such as EOS
 max_src_len: int = 300
 max_tgt_len: int = 400  # exclusive of special tokens such as EOS
 truncate_src: bool = True
 truncate_tgt: bool = True
 min_dec_steps: int = 30
 max_dec_steps: int = 500
-enc_rnn_dropout: float = 0.3
+enc_rnn_dropout: float = 0.4
 enc_attn: bool = True
 dec_attn: bool = True
 dec_in_dropout = 0
@@ -49,7 +50,7 @@ learning_rate = 0.005
 lr_decay = 0.001
 initial_accumulator_value = 0.1
 epochs = 10
-batch_size = 5
+batch_size = 8
 coverage = False
 fine_tune = False
 scheduled_sampling = False
@@ -58,9 +59,6 @@ max_grad_norm = 2.0
 is_cuda = True
 DEVICE = torch.device("cuda" if is_cuda else "cpu")
 LAMBDA = 1
-
-
-
 
 
 # Beam search
