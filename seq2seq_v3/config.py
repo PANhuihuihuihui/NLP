f0302b05b8b77f68bd5224d5b4229e284f32fc2d
Pan Huijie <phjhk@slurm-q01-inst-1.gpufarm.cs.hku.hk>
2021-03-18 15:26:47 +0800

    A

--- seq2seq_v3/config.py
+++ seq2seq_v3/config.py
@@ -5,8 +5,8 @@ from typing import Optional
 import torch
 
 # General
-hidden_size: int = 250
-dec_hidden_size: Optional[int] = 250
+hidden_size: int = 400
+dec_hidden_size: Optional[int] = 400
 embed_size: int = 100 # should not change if you load word2vec pretrain
 pointer = True
 
@@ -20,7 +20,7 @@ reduce_state_save_name = "reduce_{}".format(hidden_size)
 vocab_save_name = "vocab_{}".format(hidden_size)
 
 # Data
-max_vocab_size = 25000
+max_vocab_size = 30000
 embed_file: Optional[str] = "files/pretrained_w2v"  # use pre-trained embeddings
 source = 'train'    # use value: train or  big_samples 
 data_path: str = 'files/{}.txt'.format(source)
@@ -36,9 +36,9 @@ max_dec_steps: int = 500
 enc_rnn_dropout: float = 0.5
 enc_attn: bool = True
 dec_attn: bool = True
-dec_in_dropout = 0
-dec_rnn_dropout = 0
-dec_out_dropout = 0
+dec_in_dropout = 0.2
+dec_rnn_dropout = 0.2
+dec_out_dropout = 0.2
 
 
 # Training
@@ -50,8 +50,8 @@ learning_rate = 0.005
 lr_decay = 0.001
 initial_accumulator_value = 0.1
 epochs = 10
-batch_size = 8
-coverage = False
+batch_size = 4
+coverage = True
 fine_tune = False
 scheduled_sampling = False
 weight_tying = False
