912786e2bf0fbcf8cea065518c13d407c805fade
PANhuihuihuihui <phjhk@connect.hku.hk>
2021-03-20 20:57:42 +0800

    A

--- SAMA/reproduce.py
+++ SAMA/reproduce.py
@@ -91,7 +91,8 @@ if __name__ == "__main__":
         param_count += param.view(-1).size()[0]
     print('total number of parameters of complete model: %d\n' % param_count)
 
-    BLUE_score_1, BLUE_score_2, BLUE_score_3, BLEU4 = evaluate(
-        data, model, data.result_dir + "/" + "generation.txt")
-
+    # BLUE_score_1, BLUE_score_2, BLUE_score_3, BLEU4 = evaluate(
+    #     data, model, data.result_dir + "/" + "generation.txt")
+    
+    evaluate(data, model, data.result_dir + "/" + "generation.txt")
     gc.collect()
--- SAMA/util/evaluate.py
+++ SAMA/util/evaluate.py
@@ -3,6 +3,7 @@ import time
 import codecs
 from nltk.translate.bleu_score import corpus_bleu
 from util.makedata import buildDataperBatch
+from rouge import Rouge
 
 
 def makeResult(data, tgtOutput, instance):
@@ -77,3 +78,57 @@ def evaluate(data, model, path):
     print("time: %.2fs, speed: %.2fst/s, bleu1: %f, bleu2: %f, bleu3: %f, bleu4: %f"
           % (decode_time, speed, bleu_score_1, bleu_score_2, bleu_score_3, bleu_score_4))
     return bleu_score_1, bleu_score_2, bleu_score_3, bleu_score_4
+
+def evaluateRouge(data, model, path):
+    instances = data.test_dataset
+    model.eval()
+    print("evaluating ... ")
+    batchSize = int(data.batch)
+    start_time = time.time()
+    instances_num = len(instances)
+    total_batch = instances_num // batchSize + 1
+    pred_result = []
+    gold_result = []
+    # make batch
+    for batch_idx in range(total_batch):
+        start = batch_idx * batchSize
+        end = (batch_idx + 1) * batchSize
+        if end > instances_num:
+            end = instances_num
+        intance = instances[start:end]
+        if len(intance) == 0:
+            continue
+        # build data tensor
+        src_tensor, tgt_tensor, skilltgt_tensor, skillnet_tensor, src_lengths, tgt_lengths, \
+        skill_tgt_lengths, skill_net_lengths = buildDataperBatch(intance, data.device, data.ifGPU)
+        # model calculate result tensor
+        tgtOutput, skill = model(src_tensor, src_lengths, skillnet_tensor, skill_net_lengths)
+        # tensor to text
+        pred_words, gold_words = makeResult(data, tgtOutput, intance)
+
+        pred_result += pred_words
+        gold_result += gold_words
+
+    decode_time = time.time() - start_time
+    speed = instances_num/decode_time
+    # write to file
+    # outputFile = codecs.open(path, "w", "utf-8")
+    # for idx in range(instances_num):
+    #     for idy in range(len(gold_result[idx][0])):
+    #         outputFile.write("".join(gold_result[idx][0][idy]))
+    #     outputFile.write("\n")
+    #     for idy in range(len(pred_result[idx])):
+    #         outputFile.write("".join(pred_result[idx][idy]))
+    #     outputFile.write("\n")
+    #     outputFile.write("\n")
+    # outputFile.close()
+
+    # evaluate
+    result = rouge.get_scores(pred_result, gold_result, avg=True)
+
+    print("time: %.2fs, speed: %.2fst/s"
+          % (decode_time, speed))
+    print('rouge1: ', result['rouge-1'])
+    print('rouge2: ', result['rouge-2'])
+    print('rougeL: ', result['rouge-l'])
+    return result
