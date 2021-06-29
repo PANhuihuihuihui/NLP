378c4a03c87c5f4f437932ca5c087a8a5c21d6f2
PANhuihuihuihui <phjhk@connect.hku.hk>
2021-06-29 14:05:01 +0800

    A

--- DETR/test.py
+++ DETR/test.py
@@ -1,12 +1,13 @@
+import torch
 from torchsummary import summary
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = torch.hub.load('facebookresearch/detr', 'detr_resnet50', pretrained=True).to(device)
 
 # one method
-for name, param in model.named_parameters():
-    if param.requires_grad:
-        print (name, param.data)
+#for name, param in model.named_parameters():
+#    print (name)
 # seconde method
-summary(model,(1,3,80,1200))
+summary(model)
